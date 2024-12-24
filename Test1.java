5dc8dd69dcd884719d216a8a38ea57db845ca2d0
Renuka-13-Bhagya <renukalolabhattu13@gmail.com>
2024-12-24 16:49:16 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,11 @@
+class Test1
+{
+	public static void main(String[] args)
+	{
+		//System.out.println("welcome");
+		System.out.println(1+2);
+		System.out.println(1-6);
+		System.out.println("Renuka"+1013);
+		System.out.println("welcome");
+	}
+}
